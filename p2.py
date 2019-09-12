8198eb9be86413aa8601204c15bc4fc8c00ec02c
antocaroca <antonella.caroca@cci-entel.cl>
2019-09-11 23:07:59 -0300

    2

--- p2.py
+++ p2.py
@@ -3,36 +3,36 @@ entregue una lista con los nombres de los pacientes que
  se atendieron en los dias señalados
 """
 nombres = {}
-pacientes = {}
-pacientes_atencion = {}
-archivos = ("atenciones.txt", "pacientes.txt")
+fechas = {}
+archivos = ("pacientes.txt", "atenciones.txt")
 
 def pacientes_dia(dia, mes, ano):
+    
+    lista_rut = []
+    date = (f"{dia}-{mes}-{ano}")
+
     archivo_atenciones = open("atenciones.txt", "r")
-    archivo_pacientes = open("pacientes.txt", "r")
-
-    date = f"{dia}-{mes}-{ano}"
-    rut_atenciones = []
-
     for line in archivo_atenciones:
         rut, fecha, precio = line.strip().split(":")
+        if fecha not in fechas:
+            fechas[fecha]=[]
+        fechas[fecha].append(rut)
+    #print(fechas) # dicc fecha:rut
+    for fecha, rut in fechas.items(): # aca se obtiene la fecha y rut segun fecha
         if fecha == date:
-            rut_atenciones.append(rut)
-    # print(rut_atenciones)
-
-    rut_pacientes = []
-    
+            #print(fecha, rut)
+            lista_rut.append(rut)
+            break
+        else:
+            print("[]")
+            break
+    archivo_atenciones.close()
+    archivo_pacientes = open("pacientes.txt")
     for linea in archivo_pacientes:
-        rut, nombre, edad = linea.strip().split(":")
-        rut_pacientes.append((rut, nombre))
-    print(rut_pacientes)
-
-    # for i in rut_pacientes: #lista rut y nombre
-    #     print(i)
-        # for j in rut_atenciones:
-        #     if (i[0]) == j:
-        #         print(i[1])
-        
+        rut,nombre,edad = linea.strip().split(":")
+    print(rut, nombre,edad)
 
     
-pacientes_dia(11,10,2019)
+
+                            
+pacientes_dia(4, 5, 2010)
